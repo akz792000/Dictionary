99962f2705eff4370650ade96506092775fe9e81
akz792000 <akz792000@gmail.com>
2025-06-23 13:54:27 +0200

    Commit 464

--- python/missing-number.py
+++ python/missing-number.py
@@ -2,13 +2,13 @@ import json
 
 # Function to find the missing ID from 1 to 3000
 def find_missing_id(ids):
-    all_ids = set(range(1, 3001))  # Create a set of all IDs from 1 to 3000
+    all_ids = set(range(10001, 11128))  # Create a set of all IDs from 1 to 3000
     ids_set = set(ids)  # Convert the list of IDs to a set
     missing_ids = all_ids - ids_set  # Find the missing IDs
     return missing_ids
 
 # Load JSON data
-with open('../File_0.json', 'r', encoding='utf-8') as file:
+with open('../File_2.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 # Extract all IDs from the JSON data
